8cdaacf2e2bdc56da944c8cfd423fe9b11249ef2
minayaserrano <minayaserrano@gmail.com>
2014-04-14 20:46:25 +0200

    Fix typo

--- lib/custom.php
+++ lib/custom.php
@@ -21,7 +21,6 @@ class custom_post_type_page_template {
         if ( is_singular() && !is_page() ) {
             $id = get_queried_object_id();
             $new_template = 'template-' . get_post_type() . $product_template_name . '.php';
-            var_dump($new_template);
             if ( $new_template && file_exists(get_query_template( 'page', $new_template )) ) {
                 $wp_query->is_page = 1;
                 $templates[] = $new_template;
