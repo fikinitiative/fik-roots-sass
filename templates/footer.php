19fc1029920d775651e8d9f559b59f8796529b02
Bill Murphy <murphy@litio.net>
2014-05-21 16:51:06 +0200

    Adding footer sidebars

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,9 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <?php dynamic_sidebar('sidebar-footer-two'); ?>
+    <?php dynamic_sidebar('sidebar-footer-three'); ?>
+    <?php dynamic_sidebar('sidebar-footer-four'); ?>
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
   </div>
 </footer>
