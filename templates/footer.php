fd7e0f1af28455a6276b241bbf26806ad5649f3a
minayaserrano <minayaserrano@gmail.com>
2014-05-21 17:41:40 +0200

    Add footer menu zone

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,11 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <?php
+        if (has_nav_menu('footer_menu')) :
+            wp_nav_menu(array('theme_location' => 'footer_menu', 'menu_class' => 'nav nav-pills'));
+        endif;
+    ?>
     <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
   </div>
 </footer>
