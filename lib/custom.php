4a2ea0eecf25f503544ae1d59e78d51bc8f8b5ed
minayaserrano <minayaserrano@gmail.com>
2014-06-02 18:17:49 +0200

    Remove debug by default

--- lib/custom.php
+++ lib/custom.php
@@ -5,5 +5,5 @@
 
 require_once locate_template('/lib/custom/templates.php');
 require_once locate_template('/lib/custom/menus.php');
-require_once locate_template('/lib/custom/debug.php'); // Bug testing only. Not to be used on a production site
+//require_once locate_template('/lib/custom/debug.php'); // Bug testing only. Not to be used on a production site
 
