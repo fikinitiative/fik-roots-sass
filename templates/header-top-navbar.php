57aa301ff072aa2e2d5054ed9b2345606db2367e
Bill Murphy <murphy@litio.net>
2014-06-02 21:29:23 +0200

    Right menu on header

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -19,7 +19,7 @@
         endif;
         ?>
     </nav>
-    <nav class="collapse navbar-collapse" role="navigation">
+    <nav class="collapse navbar-collapse pull-right" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
