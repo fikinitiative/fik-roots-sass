d27d69dd92618a5ddb7bc8f8f455c53ce150f58c
Bill Murphy <murphy@litio.net>
2014-06-02 21:30:24 +0200

    Update minified css

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1bc18ee6ae965635d5811bdf4753aef7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3eceb92eef26708d5b454ddfb536d97e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
