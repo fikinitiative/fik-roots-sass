fa2b4a7e6bcee74b3d24053b50b02d8cc267eed5
Bill Murphy <murphy@litio.net>
2014-06-04 16:17:42 +0200

    Add rows to our footer columns

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,11 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer'); ?></div>
-    <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer-two'); ?></div>
-    <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer-three'); ?></div>
-    <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer-four'); ?></div>
+    <div class="row">
+      <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer'); ?></div>
+      <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer-two'); ?></div>
+      <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer-three'); ?></div>
+      <div class="col-sm-3"><?php dynamic_sidebar('sidebar-footer-four'); ?></div>
+    </div>
   </div>
 </footer>
 <footer class="closing">
