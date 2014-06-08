b5a9bc12560653b5971a1ac0d49cf7c22d10051e
Bill Murphy <murphy@litio.net>
2014-06-08 13:38:56 +0200

    Update css

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3eceb92eef26708d5b454ddfb536d97e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6a5f6d91559fd395f1dba21f0fab3766');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
