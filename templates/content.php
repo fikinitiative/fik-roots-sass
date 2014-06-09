179de0b819a5e4701be5d54828b04975c733cfdc
Bill Murphy <murphy@litio.net>
2014-06-09 18:09:16 +0200

    Container for header

--- templates/content.php
+++ templates/content.php
@@ -1,6 +1,13 @@
 <article <?php post_class(); ?>>
+	<?php 
+
+
+    the_post_thumbnail( $size, array('class' => "img-responsive",) ) ?>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  	<div class="top">
+    	<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    	<span class="coment-count"><?php comments_number('(No Comments)', '(One Comment)', '(% Comments)' );?></span>
+	</div>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
