97390b7dbd74f02232cea785db9895e8574e3723
Bill Murphy <murphy@litio.net>
2014-06-09 18:10:24 +0200

    Rom and sidebars for blog

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,8 @@
-<?php dynamic_sidebar('sidebar-primary'); ?>
+<div class="row">
+<?php if(is_single()){ ?>
+<?php dynamic_sidebar('sidebar-blog-post'); ?>
+<?php }else{ ?>
+<?php dynamic_sidebar('sidebar-blog-main'); ?>
+<?php } ?>
+</div>
+
